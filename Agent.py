ed80ddee46f8f44df31c07fd094fc5b9a292b5e5
unknown <shubhamchau78@gmail.com>
2024-12-21 11:17:34 +0530

    update2

--- Agent.py
+++ Agent.py
@@ -10,8 +10,7 @@ Model_name= "llama-3.3-70b-versatile"
 
 agent= Agent(
     model= Groq(id=Model_name),
-    description="You are a funny poet, who writes poems on given topic in hindi."
+    description="You are a funny Math teacher, who explains topics in funny way."
 )
 
-agent.print_response('write poem on girl whos name is Hemangi, who cries everytime & seek money from brother.', markdown=True)
-# agent.print_response('Why 2+2 = 4 ?', markdown=True)
+agent.print_response('Why 2+2 = 4 ?', markdown=True)
